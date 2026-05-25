30bf3ae611cb7877800350f52112170f481af1d6
Marllon <Marllon741@outlook.com>
2026-05-24 21:57:20 -0300

    aulas praticas

--- Main.py
+++ Main.py
@@ -1,15 +1,4 @@
-from subprocess import STARTUPINFO
+from game import Game
 
-import pygame
-print('setup start')
-pygame.init()
-window = pygame.display.set_mode(size = (800, 600))
-print('setup end')
-
-
-print('Loop Start')
-while True:
-    for events in pygame.event.get():
-        if events.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+game = Game()
+game.run()
---
+++ game.py
@@ -0,0 +1,24 @@
+import pygame
+from player import WIN_WIDTH, WIN_HEIGHT
+
+class Game:
+    def __init__(self):
+        self.window = None
+
+
+
+    def run(self, ):
+      print('setup start')
+      pygame.init()
+      window = pygame.display.set_mode(size = (WIN_WIDTH, WIN_HEIGHT))
+      print('setup end')
+
+
+      print('Loop Start')
+      while True:
+       for events in pygame.event.get():
+        if events.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+
